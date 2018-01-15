90d640ce97e86d69b5e2951dc751c4e2ce519015
Aditia Darmadi Jiwandono <aditiadarmadij@gmail.com>
2018-01-15 14:36:17 +0700

    Completed level 0

---
+++ index.js
@@ -0,0 +1,11 @@
+console.log('-----Level 0-----')
+var A = 500
+var B = 600
+
+if (A > B) {
+  console.log(`A lebih besar dari B`)
+} else {
+  console.log(`A lebih kecil dari B`)
+}
+
+console.log('-----Level 0-----')
